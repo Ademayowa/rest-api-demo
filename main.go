0d97d1d0d9d2b274e823ca63e0bc9ddc243c7772
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-24 20:29:54 +0100

    chore: install gin and SQLite

---
+++ main.go
@@ -0,0 +1,20 @@
+// main.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/jobs", getJobs)
+	server.Run(":8080")
+}
+
+func getJobs(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello Job board!"})
+}
