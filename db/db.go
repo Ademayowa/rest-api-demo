4e8d5724abc37efac94445e6385368a3e40bedcb
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-28 16:43:50 +0100

    chore: create jobs table

---
+++ db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/glebarez/go-sqlite"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+
+	DB, err = sql.Open("sqlite", "job.db")
+	if err != nil {
+		panic("could not connect to database")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTable()
+}
+
+func createTable() {
+	createJobsTable := `
+	CREATE TABLE IF NOT EXISTS jobs (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+    description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		salary TEXT NOT NULL,
+		duties TEXT NOT NULL,
+		url TEXT NOT NULL
+	)
+	`
+	_, err := DB.Exec(createJobsTable)
+	if err != nil {
+		panic("could not create jobs table" + err.Error())
+	}
+}
