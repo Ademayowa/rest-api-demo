da7de1e612b7e64ea83b7e8212dfaf9b0056cf5f
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-29 10:17:03 +0100

    chore: update main.go file & create routes.go

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"net/http"
+	"os"
+
+	"github.com/Ademayowa/rest-api-demo/db"
+	"github.com/Ademayowa/rest-api-demo/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		println("No .env file found, using default environment variables")
+	}
+
+	db.InitDB()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	server := gin.Default()
+	routes.RegisterRoutes(server)
 
-	server.GET("/jobs", getJobs)
-	server.Run(":8080")
-}
-
-func getJobs(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello Job board!"})
+	server.Run(":" + port)
 }
