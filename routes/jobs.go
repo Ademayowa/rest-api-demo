d3928fb6d437f00a349967bdf8665525a0cb3ac0
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-30 20:53:53 +0100

    feat: get all jobs handler

--- routes/jobs.go
+++ routes/jobs.go
@@ -13,10 +13,24 @@ func createJob(context *gin.Context) {
 
 	err := context.ShouldBindJSON(&job)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse job" + err.Error()})
 		return
 	}
 
 	job.Save()
 	context.JSON(http.StatusCreated, gin.H{"message": "job created", "job": job})
+}
+
+// Fetch all jobs
+func getJobs(context *gin.Context) {
+	// Extract query parameter
+	filterTitle := context.Query("title")
+
+	jobs, total, err := models.GetAllJobs(filterTitle)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"jobs": jobs, "total": total})
 }
